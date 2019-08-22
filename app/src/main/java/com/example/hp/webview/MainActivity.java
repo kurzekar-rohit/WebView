507f1e0058d4ea4a3308b51e45e268456cdd53b3
Rohit <rkurzekar@gmail.com>
2019-08-22 17:24:56 +0530

    First Commit

---
+++ app/src/main/java/com/example/hp/webview/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.hp.webview;
+
+import android.graphics.Bitmap;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    SwipeRefreshLayout swiperefresh;
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String url = "https://www.unboundb2b.com";
+        webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new MyWebViewClient());
+        webView.loadUrl(url);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        swiperefresh = findViewById(R.id.swiperefresh);
+        swiperefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                webView.reload();
+                swiperefresh.setRefreshing(false); // Disables the refresh icon
+            }
+        });
+        progressBar = (ProgressBar) findViewById(R.id.progressBar1);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    class MyWebViewClient extends WebViewClient {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            progressBar.setVisibility(View.GONE);
+        }
+    }
+
+
+}
