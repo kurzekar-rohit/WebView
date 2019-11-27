2ae9fc98c2ba103e84308257d6fd31c281585a84
Rohit Kurzekar <kurzekar.rohit@gmail.com>
2019-11-27 15:00:23 +0530

    Url Change

--- app/src/main/java/com/example/hp/webview/MainActivity.java
+++ app/src/main/java/com/example/hp/webview/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String url = "https://www.unboundb2b.com";
+        String url = "https://techresearchonline.com/";
         webView = (WebView) findViewById(R.id.webview);
         webView.setWebViewClient(new MyWebViewClient());
         webView.loadUrl(url);
